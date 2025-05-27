d51be0ae089223aafde91460c02348901b5b599c
nne <rjnf1404@gmail.com>
2025-05-27 10:48:40 +0800

    p11

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from .views import home, about, contact, detail_artikel
-from .autentifikasi import akun_login, akun_register, user_logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,11 +13,7 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('contact', contact, name='contact'),
     path('detail/<slug:slug>', detail_artikel, name="detail_artikel"),
-    path('dashboard/', include("berita.urls")),
-
-        path('autentifikasi/akun_login', akun_login, name="akun_login"),
-    path('autentifikasi/akun_register', akun_register, name="akun_register"),
-    path('autentifikasi/login', user_logout, name="user_logout"),
+    path('dashboard/', include("berita.urls"))
 ]
 
 if settings.DEBUG: 
